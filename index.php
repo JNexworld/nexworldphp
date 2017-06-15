bdc755b7eb00b3892b67ed7ab890bf791e856549
JNexworld <johann.michard@nexworld.fr>
2017-06-15 14:16:28 +0200

    Create index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "<h1>NEXWORLD";
-echo "<br><br>Building the Future";
-echo "<br>Nexworld dispose d'une expertise qui vous aide à réinventer votre activité métier et transformer votre entreprise";
+echo "<br>Building the Future";
+echo "<br><br>Nexworld dispose d'une expertise qui vous aide à réinventer votre activité métier et transformer votre entreprise";
